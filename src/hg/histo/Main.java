a3b9fa25490f1d2a8041fdac709ed3bad4378abc
laetitia28 <laetitia.gaignier@gmail.com>
2013-10-17 10:40:13 +0200

    testFram Main

--- src/hg/histo/Main.java
+++ src/hg/histo/Main.java
@@ -1,11 +1,38 @@
 package hg.histo;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("commit oush ?");
 		System.out.println("it ayouta");
+		
+		String path ="src/images/image0046.csv";
+		File myFile = new File(path);
+		FillCellWithCSV f;
+		try {
+			f = new FillCellWithCSV(myFile);
+			List<Cell> listcells = f.allCells();// liste de toutes les cellules
+			
+			FramCell frame = new FramCell(listcells);
+			
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setSize(1000, 700);
+			
+			frame.setVisible(true);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
 	}
 
-}
+
