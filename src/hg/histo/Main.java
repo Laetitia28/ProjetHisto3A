a5bfb341194469bd7a465a236112461420209d13
laetitia28 <laetitia.gaignier@gmail.com>
2013-10-30 11:02:49 +0100

    clean hope

--- src/hg/histo/Main.java
+++ src/hg/histo/Main.java
@@ -14,34 +14,27 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-<<<<<<< HEAD
 		String path ="src/ressources/image0046.csv";
 		File myFile = new File(path);
 		FillCellWithCSV f;
 		Menu menu=new Menu();
-		try {
-			f = new FillCellWithCSV(myFile);
-			List<Cell> listcells = f.allCells();// liste de toutes les cellules
-			FramCell frame = new FramCell(listcells);
-=======
-		//String path ="src/ressources/image0046.csv";
-		//File myFile = new File(path);
-		//FillCellWithCSV f;
 		
-		
-			//f = new FillCellWithCSV(myFile);
-			//List<Cell> listcells = f.allCells();// liste de toutes les cellules
-			FramCell frame = new FramCell();
-			frame.initFrame("src/ressources/image0046.csv");
->>>>>>> b80e74f298e297c107bbd78a0d937c836f817e32
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setSize(1000, 700);
-			frame.setVisible(true);
+			try {
+				f = new FillCellWithCSV(myFile);
+				List<Cell> listcells = f.allCells();// liste de toutes les cellules
+				//List<Cell> listcells = f.allCells();// liste de toutes les cellules
+				FramCell frame = new FramCell();
+				frame.initFrame("src/ressources/image0046.csv");
+
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setSize(1000, 700);
+				frame.setVisible(true);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
-
+		}
 	
-		
-	}
-	}
 
-
+}
