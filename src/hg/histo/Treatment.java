321ce13290acc4c51042f79119715ddf3c7010be
laetitia28 <laetitia.gaignier@gmail.com>
2013-11-20 08:11:47 +0100

    add method changefile to treatement class

--- src/hg/histo/Treatment.java
+++ src/hg/histo/Treatment.java
@@ -1,6 +1,5 @@
 package hg.histo;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.io.File;
 import java.io.IOException;
@@ -10,6 +9,8 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
@@ -21,28 +22,19 @@ public class Treatment {
 	private String path_excel_default = "src/ressources/image0046.csv";
 	private String path_current = path_excel_default;
 	private String path_initial;
-	SearchFile searchFile ;
+	private ImageIcon img ;
+	private SearchFile searchFile ;
 	private HashMap<String, String> m = new HashMap<String,String>();
 
-	
-	public String getPath_image() {
-		return path_image;
-	}
-	public void setPath_image(String path_image) {
-		this.path_image = path_image;
-	}
-	public HashMap<String, String> getM() {
-		return m;
-	}
-	public void setM(HashMap<String, String> m) {
-		this.m = m;
-	}
+
+
 	List<Cell> listCells;
 
 	public Treatment (){
-		
+
 		listCells = new ArrayList<Cell>();
 		listCells = setListCell(path_current);
+		img = new ImageIcon(img_default);
 
 
 	}
@@ -56,7 +48,7 @@ public class Treatment {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			setListCell(path_excel_default);
+			setListCell(getPath_excel_default());
 		}
 		return null;
 	}
@@ -69,14 +61,12 @@ public class Treatment {
 		m.put("Nucleus DAB+", "blue");
 		//System.out.println(m.keySet());
 
-
-
 	}
 	public mxGraphComponent initFrame( mxGraph graph,  mxGraphComponent graphComponent){
 		graph.getModel().beginUpdate();
 		try
 		{
-			for (Cell c : listCells) {
+			for (Cell c : this.listCells) {
 
 				Object v1 = graph
 						.insertVertex(graph.getDefaultParent(), null, c.getClass_name(),
@@ -94,24 +84,21 @@ public class Treatment {
 		graphComponent.setBackgroundImage(img);			
 
 		return graphComponent;
-		
+
 		//getContentPane().add(graphComponent, BorderLayout.CENTER);
 	}
 	public mxGraphComponent changeFrame(String path,mxGraph graph,  mxGraphComponent graphComponent){
-
-
 		graph.setCellsDeletable(true);
 		graph.removeCells(graph.getChildVertices(graph.getDefaultParent()));
 		graph.refresh();
 		graph.setCellsDeletable(false);
-		listCells.clear();
-		listCells = setListCell(path);
+		this.listCells.clear();
+		this.listCells = setListCell(path);
 
-		
 		graph.getModel().beginUpdate();
 		try
 		{
-			for (Cell c : listCells) {
+			for (Cell c : this.listCells) {
 
 				Object v2 = graph
 						.insertVertex(graph.getDefaultParent(), null, c.getClass_name(),
@@ -127,18 +114,17 @@ public class Treatment {
 
 		//display background
 
-		ImageIcon img = new ImageIcon(path_image);
+		ImageIcon img = new ImageIcon(getPath_image());
 		img = ImageToIcon.scale(path_image, (int)(img.getIconWidth()*0.4),(int)(img.getIconHeight()*0.4));
 		graphComponent.setBackgroundImage(img);	
 		return graphComponent;
-		//getContentPane().add(graphComponent, BorderLayout.CENTER);
 
 	}
 	public void displaySelectedCells(String nameSelected,mxGraph graph){
 		graph.getModel().beginUpdate();
 		try
 		{
-			for (Cell c : listCells) {
+			for (Cell c : this.listCells) {
 				if(c.getClass_name().equals(nameSelected)){
 
 					Object v2 = graph
@@ -155,18 +141,7 @@ public class Treatment {
 		}
 
 	}
-	public String GetPath_Ext(String path_current){	
-		StringTokenizer st = new StringTokenizer(path_current, "."); 
-		String ext = "";
-		while (st.hasMoreTokens()) { 
-			path_initial = st.nextToken();
-			//System.out.println("path_initial get ext : " + path_initial); 
-			ext=st.nextToken();
-			//System.out.println("ext : " + ext);
-		}
-		return ext;
- 
-	}
+
 	public double newScale(mxGraphComponent graphComponent){
 		double newScale = 1;
 
@@ -188,42 +163,118 @@ public class Treatment {
 			newScale = Math.min((double) w / gw, (double) h / gh);
 		}
 		return newScale;
-		
+
 	}
 
-	public boolean GoodFile(String ext,String path_current){
-		boolean found= false;
-		System.out.println("Path selected currentG : " + path_current);
-		
+
+	public void ChangeFile(JFileChooser chooser,mxGraphComponent graphComponent,mxGraph graph){
+
+		path_current = chooser.getSelectedFile().getAbsolutePath();
+		System.out.println("Path selected current : " + path_current);
+
 		StringTokenizer st = new StringTokenizer(path_current, "."); 
-		
+
 		while (st.hasMoreTokens()) { 
-			 path_initial = st.nextToken();
-			System.out.println("path_initialG : " + path_initial); 
+
+			String path_initial = st.nextToken();
+			System.out.println("path_initial : " + path_initial); 
+
+			String ext=st.nextToken();
+			System.out.println("ext : " + ext);
+
+			if(ext.equals("csv")){
+				System.out.println("Ceci est un bon fichier ");
+				System.out.println(ext);
+
+				//remplace .cvs to jpg
+				String newPath1=ext.replace(ext.charAt(0), 'j');
+				String newPath2=newPath1.replace(ext.charAt(1), 'p');
+				String newPath3=newPath2.replace(ext.charAt(2), 'g');
+
+				// creation de path.jpg
+				path_image = path_initial +"."+ newPath3;
+
+				System.out.println("path_image " + path_image);
+
+				// aller chercher path.jpg dans le dossier
+
+				searchFile = new SearchFile(path_image);
+				boolean found = searchFile.searchFileImage(searchFile.name, searchFile.filePath);
+				if (found ){
+					System.out.println("ok found file .jpg ");
+					JOptionPane.showMessageDialog(graphComponent, "File .jpn  found ! Name is :"+path_image,"avertissement",
+							JOptionPane.WARNING_MESSAGE);
+
+				}
+				else {
+					JOptionPane.showMessageDialog(graphComponent, "File .jpn not found ! Default image is image0046 ",
+							"avertissement",
+							JOptionPane.ERROR_MESSAGE);
+					System.out.println("Ko not found .jpg ");
+					path_image = img_default;
+				}
+
+				//if .cvs is found then display new graph
+				System.out.println("path using with change Frame"+ path_current);
+				changeFrame(path_current,graph,  graphComponent);
+
+			}
+
+			else if (!ext.equals("csv")){
+				System.out.println("Ceci n'est pas un bon fichier ");
+
+				JOptionPane.showMessageDialog(graphComponent, "File choosen is not expected",
+						"avertissement",
+						JOptionPane.ERROR_MESSAGE);
+
+
+			}
+
 		}
-
-		if(GetPath_Ext(path_current).equals("csv")){
-			System.out.println("Ceci est un bon fichier ");
-			System.out.println(ext);
-
-			//remplace .cvs to jpg
-			String newPath1=ext.replace(ext.charAt(0), 'j');
-			String newPath2=newPath1.replace(ext.charAt(1), 'p');
-			String newPath3=newPath2.replace(ext.charAt(2), 'g');
-
-			// creation de path.jpg
-			path_image = path_initial +"."+ newPath3;
-
-			System.out.println("path_image " + path_image);
-
-			// aller chercher path.jpg dans le dossier
-			searchFile = new SearchFile(path_image);
-			found = searchFile.searchFileImage(searchFile.name, searchFile.filePath);
-			
-		}
-		
-		return found;
+	}
+	public String getImg_default() {
+		return img_default;
+	}
+	public void setImg_default(String img_default) {
+		this.img_default = img_default;
+	}
+	public String getPath_excel_default() {
+		return path_excel_default;
+	}
+	public void setPath_excel_default(String path_excel_default) {
+		this.path_excel_default = path_excel_default;
+	}
+	public String getPath_current() {
+		return path_current;
+	}
+	public void setPath_current(String path_current) {
+		this.path_current = path_current;
+	}
+	public String getPath_initial() {
+		return path_initial;
+	}
+	public void setPath_initial(String path_initial) {
+		this.path_initial = path_initial;
+	}
+	public ImageIcon getImg() {
+		return img;
+	}
+	public void setImg(ImageIcon img) {
+		this.img = img;
 	}
 
+	public String getPath_image() {
+		return path_image;
+	}
+	public void setPath_image(String path_image) {
+		this.path_image = path_image;
+	}
+
+	public HashMap<String, String> getM() {
+		return m;
+	}
+	public void setM(HashMap<String, String> m) {
+		this.m = m;
+	}
 }
 
