d3f5f54d95abdc3827024197f64fb80110112915
laetitia28 <laetitia.gaignier@gmail.com>
2013-12-10 20:31:15 +0100

    Add getter and setter

--- src/hg/histo/SearchFile.java
+++ src/hg/histo/SearchFile.java
@@ -4,9 +4,57 @@ import java.io.File;
 
 public class SearchFile {
 
-	String path ;
+	private String path ;
 	String name;
 	File filePath ; 
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public File getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(File filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getPath_default() {
+		return path_default;
+	}
+
+	public void setPath_default(String path_default) {
+		this.path_default = path_default;
+	}
+
+	public String getName_default() {
+		return name_default;
+	}
+
+	public void setName_default(String name_default) {
+		this.name_default = name_default;
+	}
+
+	public File getFilePath_default() {
+		return filePath_default;
+	}
+
+	public void setFilePath_default(File filePath_default) {
+		this.filePath_default = filePath_default;
+	}
+
 	String path_default = "C:/Users/Laetitia/Documents/Projet_Histo/image0046.csv" ;
 	String name_default =  "image0046.csv";
 	File filePath_default = new File("C:/Users/Laetitia/Documents/Projet_Histo"); 
