48ed7ce53b7a4d71559894dc7daddf39dd02246c
laetitia28 <laetitia.gaignier@gmail.com>
2014-02-06 21:31:29 +0100

    MainTest for RequestFram

---
+++ src/hg/hist/MVC/MainLaetitiaTest.java
@@ -0,0 +1,16 @@
+package hg.hist.MVC;
+
+import javax.swing.JFrame;
+
+public class MainLaetitiaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RequestFram  frame2 = new RequestFram(10079 , 0.765,580,626,0.391,128);
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame2.setSize(1200, 700);
+		frame2.setVisible(true);
+	}
+
+}
